4d29402e1429c084bf875f1a792b87571eb3a2fc
Aji Kamaludin <aji19kamaludin@gmail.com>
2018-10-09 23:19:00 +0700

    nambah seo dikit

--- view/header_index.php
+++ view/header_index.php
@@ -7,7 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
+    <meta name="Description" content="<?= 'aji kamaludin '.getPengaturan()['deskripsi'].' '.getPengaturan()['nama'];?>" />
+    <meta name="Keywords" content="<?= 'aji kamaludin '.getPengaturan()['deskripsi'].' '.getPengaturan()['nama'];?>" />
     <!-- Title -->
     <title><?= getPengaturan()['nama'];?></title>
 
